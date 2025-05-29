dd8a911a538396e6260bb8b708edee49735c9774
崔江枫 <18814321456@163.com>
2025-05-29 16:12:37 +0800

    refactor(oem): 重构 API 提供者并更新日志配置
    - 移除 Application 类中的 ComponentScan 注解
    - 更新 HelloServiceApi 类，使用 DubboService 注解替换 Service 注解
    - 在 application.yml 中添加 Dubbo 日志配置
    - 删除 application.yml 中的 scan 配置

--- user-service/oem/consumer/src/main/java/top/cjf_rb/oem/Application.java
+++ user-service/oem/consumer/src/main/java/top/cjf_rb/oem/Application.java
@@ -3,13 +3,8 @@ package top.cjf_rb.oem;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import top.cjf_rb.aut_interface.annotation.AutoDubboService;
 
 @SpringBootApplication
-@ComponentScan(
-        excludeFilters = @ComponentScan.Filter(classes = {AutoDubboService.class})
-)
 @EnableDubbo
 public class Application {
     public static void main(String[] args) {
